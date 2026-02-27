3cb60399ab0c1e484b14f7766fa8222126f5167d
almamunrifat47-design <almamunrifat47@gmail.com>
2026-02-27 15:07:14 +0600

    Add files via upload

--- api/generate.js
+++ api/generate.js
@@ -1,1 +1,40 @@
+import fs from "fs";
+import path from "path";
+import PizZip from "pizzip";
+import Docxtemplater from "docxtemplater";
 
+export default function handler(req, res) {
+
+  if (req.method !== "POST") {
+    return res.status(405).send("Method Not Allowed");
+  }
+
+  const templatePath = path.join(process.cwd(), "public", "template.docx");
+  const content = fs.readFileSync(templatePath, "binary");
+
+  const zip = new PizZip(content);
+  const doc = new Docxtemplater(zip);
+
+  doc.setData(req.body);
+
+  try {
+    doc.render();
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+
+  const buffer = doc.getZip().generate({
+    type: "nodebuffer",
+  });
+
+  res.setHeader(
+    "Content-Type",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+  );
+  res.setHeader(
+    "Content-Disposition",
+    "attachment; filename=Assignment_Cover.docx"
+  );
+
+  res.send(buffer);
+}
